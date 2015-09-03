a867deab61d966354f0b8cfef693d527ef36f4b9
shumy <micaelpedrosa@gmail.com>
2015-09-03 10:18:50 +0100

    Litle changes on setup

--- org.molymer.conf.ui/src-gen/org/molymer/conf/ui/contentassist/AbstractConfigDslProposalProvider.java
+++ org.molymer.conf.ui/src-gen/org/molymer/conf/ui/contentassist/AbstractConfigDslProposalProvider.java
@@ -53,7 +53,4 @@ public class AbstractConfigDslProposalProvider extends org.eclipse.xtext.common.
 	public void complete_QualifiedName(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
 		// subclasses may override
 	}
-	public void complete_STRING(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
-		// subclasses may override
-	}
 }
